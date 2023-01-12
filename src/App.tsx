4534a63a0dd7fb3e9e1ee48bc52d59414fb0bc36
raman baral <ramanisstudent@gmail.com>
2023-01-12 21:59:28 +0545

    added footer with link to portfolio website

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./App.css";
 
 import QuoteBox from "./components/QuoteBox/QuoteBox";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <QuoteBox />
+    <React.Fragment>
+      <QuoteBox />
+      <Footer />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <span>
+        built by{" "}
+        <a
+          href="https://ramanbaral.com.np/"
+          id="footer-author"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @raman_baral
+        </a>
+      </span>
+    </footer>
+  );
+}
+
+export default Footer;
