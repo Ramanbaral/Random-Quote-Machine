2e246981b1cae615ef1e5d925099d86dfa046035
raman baral <ramanisstudent@gmail.com>
2023-01-12 17:22:07 +0545

    finished tweet & new quote functionality

--- src/components/QuoteBox/QuoteBox.tsx
+++ src/components/QuoteBox/QuoteBox.tsx
@@ -1,16 +1,58 @@
-import React, { useState } from "react";
-import './QuoteBox.css';
+import React, { useEffect, useState } from "react";
+import "./QuoteBox.css";
 import TwitterIcon from "../../img/twitter.png";
+import QuoteLeft from "../../img/quote-left.svg";
+import Quotes from "./quotes.json";
 
 function QuoteBox() {
+  let [currentQuote, setCurrentQuote] = useState("");
+  let [currentAuthor, setCurrentAuthor] = useState("");
+  let [currentColor, setCurrentColor] = useState("");
+
+  const TWEETLINK = "https://twitter.com/intent/tweet?hashtags=quotes&text=" +
+  encodeURIComponent('"' + currentQuote + '"\n- ' + currentAuthor + '\n')
+
+  let colors = [
+    "#C7EAE4",
+    "#311E10",
+    "#EFCA08",
+    "#E2AFDE",
+    "#A7E8BD",
+    "#FFD972",
+    "#55DDE0",
+    "#2F4858",
+    "#EBB3A9",
+    "#38A3A5",
+    "#684A52",
+    "#2A2B2A",
+    "#706C61",
+  ];
+
+  function newQuote() {
+    getRandomQuote();
+
+  }
+
+  function getRandomQuote() {
+    const quote =
+      Quotes.quotes[Math.floor(Math.random() * Quotes.quotes.length)];
+    setCurrentQuote(quote.quote);
+    setCurrentAuthor(quote.author);
+  }
+
+  useEffect(() => {
+    getRandomQuote();
+  }, []);
+
   return (
     <div id="quote-box">
-      <p id="text">Education costs money. But then so does ignorance.</p>
-      <span id="author">- Sir Claus Moser</span>
+      <img src={QuoteLeft} alt="Quote Left" style={{fontSize: "16px" }} />
+      <p id="text">{currentQuote}</p>
+      <span id="author">- {currentAuthor}</span>
       <div className="btns">
         <button>
           <a
-            href="https://twitter.com/intent/tweet"
+            href={TWEETLINK}
             id="tweet-quote"
             target="_blank"
             rel="noreferrer"
@@ -18,7 +60,7 @@ function QuoteBox() {
             <img src={TwitterIcon} alt="Twitter icon" />
           </a>
         </button>
-        <button id="new-quote">New quote</button>
+        <button id="new-quote" onClick={newQuote}>New quote</button>
       </div>
     </div>
   );
