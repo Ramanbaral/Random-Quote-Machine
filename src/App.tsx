45241bf69bb0d452612938dd9c73678e605f416f
raman baral <ramanisstudent@gmail.com>
2023-01-12 08:03:05 +0545

    finished writing html and downloaded twitter icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div id="quote-box">
+      <p id="text">Education costs money. But then so does ignorance.</p>
+      <span id="author">- Sir Claus Moser</span>
+      <div className="btns">
+        <button><a href="https://twitter.com/intent/tweet" id="tweet-quote" target="_blank" rel="noreferrer">Twitter Icon</a></button>
+        <button id="new-quote">New quote</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
