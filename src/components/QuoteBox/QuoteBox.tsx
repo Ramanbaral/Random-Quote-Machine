110350721b756105c5a3d437c513157922cff78a
raman baral <ramanisstudent@gmail.com>
2023-01-13 12:05:31 +0545

    added fade animation in body

--- src/components/QuoteBox/QuoteBox.tsx
+++ src/components/QuoteBox/QuoteBox.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./QuoteBox.css";
 import TwitterIcon from "../../img/twitter.png";
-import QuoteLeft from "../../img/quote-left.svg";
 import Quotes from "./quotes.json";
 
 function QuoteBox() {
@@ -54,7 +53,15 @@ function QuoteBox() {
   return (
     <div id="quote-box">
       <p id="text" style={{ color: currentColor }}>
-        <svg id="quote-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{"fill": currentColor}}> <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"/></svg>
+        <svg
+          id="quote-left"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          style={{ fill: currentColor }}
+        >
+          {" "}
+          <path d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z" />
+        </svg>
         {currentQuote}
       </p>
       <span id="author" style={{ color: currentColor }}>
@@ -63,7 +70,7 @@ function QuoteBox() {
       <div className="btns">
         <button style={{ backgroundColor: currentColor }}>
           <a href={TWEETLINK} id="tweet-quote" target="_blank" rel="noreferrer">
-            <img src={TwitterIcon} alt="Twitter icon" style={{"width": 24}} />
+            <img src={TwitterIcon} alt="Twitter icon" style={{ width: 24 }} />
           </a>
         </button>
 
